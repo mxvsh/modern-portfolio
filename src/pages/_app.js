639bd6c7afd730ff946db58d8137853bc15cf285
xencodes <xencodes@icloud.com>
2022-05-06 10:57:13 +0530

    show full name in title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,15 @@
 import '../../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
+
+import { about } from 'constants/about';
 
 function ModernPortfolio({ Component, pageProps }) {
 	return (
 		<ChakraProvider>
+			<Head>
+				<title>{about.full_name}</title>
+			</Head>
 			<Component {...pageProps} />
 		</ChakraProvider>
 	);
